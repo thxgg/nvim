5707c74fc52f95a42f5de6cc31bb34c2c6bba454
Georgi Georgiev <gatanasovgeorgiev@gmail.com>
2023-11-03 00:34:50 +0100

    feat: add options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,13 +1,35 @@
-vim.opt.number = true
-vim.opt.breakat = " \t!@*-+;:,./?"
-vim.opt.breakindent = true
-vim.opt.breakindentopt = 'shift:2,sbr'
-vim.opt.showbreak = '↪'
-vim.opt.clipboard = "unnamedplus"
-vim.opt.expandtab = true
-vim.opt.guicursor = ""
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-vim.opt.linebreak = true
-vim.opt.list = true
-vim.opt.listchars = "tab:» ,eol:↩,trail:-"
+local opt = vim.opt
+
+opt.backup = false
+opt.breakat = " \t!@*-+;:,./?"
+opt.breakindent = true
+opt.breakindentopt = 'shift:2,sbr'
+opt.clipboard = "unnamedplus"
+opt.expandtab = true
+opt.guicursor = ""
+opt.hlsearch = false
+opt.incsearch = true
+opt.isfname:append("@-@")
+opt.linebreak = true
+opt.list = true
+opt.listchars = "tab:» ,eol:↲,trail:-"
+opt.number = true
+opt.relativenumber = false
+opt.scrolloff = 8
+opt.shiftwidth = 4
+opt.showbreak = '↳'
+opt.signcolumn = "yes"
+opt.smartindent = true
+opt.softtabstop = 4
+opt.splitbelow = true
+opt.splitright = true
+opt.swapfile = false
+opt.tabstop = 4
+opt.termguicolors = true
+opt.undofile = true
+opt.wrap = true
+opt.wrapmargin = 2
+
+-- Right click mouse menu
+vim.cmd.aunmenu{'PopUp.How-to\\ disable\\ mouse'}
+vim.cmd.aunmenu{'PopUp.-1-'}
