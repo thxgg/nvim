f1f351705fde618595bb9a7d06e63518a4b9a854
Georgi Georgiev <gatanasovgeorgiev@gmail.com>
2023-11-15 01:02:47 +0100

    chore: bump lazy-lock

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -13,11 +13,11 @@ return {
         },
     },
     keys = {
-        { '<leader>xd',  '<CMD>Trouble document_diagnostics',     desc = "Document diagnostics" },
-        { '<leader>xw',  '<CMD>Trouble workspace_diagnostics',    desc = "Workspace diagnostics" },
-        { '<leader>gd',  '<CMD>Trouble lsp_definitions<CR>',      desc = '[G]o to [D]efinition' },
-        { '<leader>gtd', '<CMD>Trouble lsp_type_definitions<CR>', desc = '[G]o to [T]ype [D]efinition' },
-        { '<leader>gr',  '<CMD>Trouble lsp_references<CR>',       desc = '[G]o to [R]eferences' },
-        { '<leader>gi',  '<CMD>Trouble lsp_implementations<CR>',  desc = '[G]o to [I]mplementations' },
+        { '<leader>xd',  '<CMD>Trouble document_diagnostics<CR>',  desc = "Document diagnostics" },
+        { '<leader>xw',  '<CMD>Trouble workspace_diagnostics<CR>', desc = "Workspace diagnostics" },
+        { '<leader>gd',  '<CMD>Trouble lsp_definitions<CR>',       desc = '[G]o to [D]efinition' },
+        { '<leader>gtd', '<CMD>Trouble lsp_type_definitions<CR>',  desc = '[G]o to [T]ype [D]efinition' },
+        { '<leader>gr',  '<CMD>Trouble lsp_references<CR>',        desc = '[G]o to [R]eferences' },
+        { '<leader>gi',  '<CMD>Trouble lsp_implementations<CR>',   desc = '[G]o to [I]mplementations' },
     }
 }
