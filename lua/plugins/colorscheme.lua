8847313d408f32d903e5743e9470c1ee4abecd92
Georgi Georgiev <gatanasovgeorgiev@gmail.com>
2024-02-26 11:48:36 +0100

    chore: bump lazy-lock

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,9 +4,23 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
+            -- vim.opt.background = 'dark'
+            -- vim.g.gruvbox_material_better_performance = 1
+            -- vim.cmd.colorscheme 'gruvbox-material'
+        end
+    },
+    {
+        'navarasu/onedark.nvim',
+        lazu = false,
+        priority = 1000,
+        opts = {
+            style = 'cool',
+        },
+        config = function(_, opts)
+            require('onedark').setup(opts)
+
             vim.opt.background = 'dark'
-            vim.g.gruvbox_material_better_performance = 1
-            vim.cmd.colorscheme 'gruvbox-material'
+            vim.cmd.colorscheme 'onedark'
         end
     }
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
         options = {
-            theme = 'gruvbox-material',
+            theme = 'onedark',
             section_separators = '',
             component_separators = {
                 left = '|',
