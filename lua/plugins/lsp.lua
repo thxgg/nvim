43e631fdb179f06bfb1b0225223fdbb355a16a7e
Georgi Georgiev <gatanasovgeorgiev@gmail.com>
2024-06-02 01:06:17 +0200

    add tailwind sorter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -408,4 +408,10 @@ return {
       }
     end,
   },
+  {
+    'laytan/tailwind-sorter.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
+    build = 'cd formatter && npm ci && npm run build',
+    config = true,
+  }
 }
