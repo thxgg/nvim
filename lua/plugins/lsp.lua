041fe335c1e11fb759a172e009d487672c2fc803
Georgi Georgiev <gatanasovgeorgiev@gmail.com>
2024-07-22 15:53:15 +0300

    add markdown format on save

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,7 +7,7 @@ end
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = false })
 local on_attach = function(client, bufnr)
-  if client.supports_method("textDocument/formatting") then
+  if client.supports_method("textDocument/formatting") or vim.bo.filetype == "markdown" then
     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
     vim.api.nvim_create_autocmd("BufWritePre", {
       group = augroup,
@@ -488,6 +488,9 @@ return {
         nls.builtins.formatting.google_java_format,
         nls.builtins.formatting.sqlfluff.with({
           extra_args = { "--dialect", "postgres" },
+        }),
+        nls.builtins.formatting.prettierd.with({
+          filetypes = { "json", "jsonc", "yaml", "markdown", "markdown.mdx" },
         }),
 
         -- Code Actions
