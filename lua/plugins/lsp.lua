a583693719bfb4890cb7f9280c4a0f867e4a601f
Georgi Georgiev <gatanasovgeorgiev@gmail.com>
2024-09-20 19:00:03 +0300

    feat: add sonarlint

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -474,28 +474,6 @@ return {
 
 			-- Avoid race condition by calling attach the first time, since the autocmd won't fire.
 			attach_jdtls()
-
-			-- Setup SonarLint
-			require("sonarlint").setup({
-				server = {
-					cmd = {
-						"sonarlint-language-server",
-						-- Ensure that sonarlint-language-server uses stdio channel
-						"-stdio",
-						"-analyzers",
-						-- paths to the analyzers you need, using those for python and java in this example
-						vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
-						vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarcfamily.jar"),
-						vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
-					},
-				},
-				filetypes = {
-					-- Tested and working
-					"python",
-					"cpp",
-					"java",
-				},
-			})
 		end,
 	},
 	{
@@ -542,6 +520,32 @@ return {
 			on_save_enabled = true,
 			on_save_pattern = { "*.html", "*.jsx", "*.tsx", "*.vue" },
 			trim_spaces = true,
+		},
+	},
+	{
+		"https://gitlab.com/schrieveslaach/sonarlint.nvim.git",
+		opts = {
+			server = {
+				cmd = {
+					"sonarlint-language-server",
+					"-stdio",
+					"-analyzers",
+					vim.fn.expand("~/.local/share/nvim/mason/share/sonarlint-analyzers/sonarjava.jar"),
+				},
+				settings = {
+					sonarlint = {
+						rules = {
+							["typescript:S101"] = { level = "on", parameters = { format = "^[A-Z][a-zA-Z0-9]*$" } },
+							["typescript:S103"] = { level = "on", parameters = { maximumLineLength = 180 } },
+							["typescript:S106"] = { level = "on" },
+							["typescript:S107"] = { level = "on", parameters = { maximumFunctionParameters = 7 } },
+						},
+					},
+				},
+			},
+			filetypes = {
+				"java",
+			},
 		},
 	},
 }
