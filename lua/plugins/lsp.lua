e01647a12a99fd18f6318c0117bb25117e34848d
Georgi Georgiev <gatanasovgeorgiev@gmail.com>
2024-09-20 06:34:23 +0300

    feat: enable tailwind-sorter on save

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -518,6 +518,10 @@ return {
 		"laytan/tailwind-sorter.nvim",
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
 		build = "cd formatter && npm ci && npm run build",
-		config = true,
+		opts = {
+			on_save_enabled = true,
+			on_save_pattern = { "*.html", "*.jsx", "*.tsx", "*.vue" },
+			trim_spaces = true,
+		},
 	},
 }
